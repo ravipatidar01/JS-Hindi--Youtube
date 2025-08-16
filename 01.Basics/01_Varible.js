be6fd0a7922b68860db6996f6cfbbb11a25b1c0a
ravipatidar01 <ravipatidar6266@gmail.com>
2025-08-16 05:26:23 +0000

    varible js bnaya

---
+++ 01.Basics/01_Varible.js
@@ -0,0 +1,19 @@
+const accountId = 12345
+let   accountEmail = "ravipatidar@gmail.com"
+var  accountPassword =  "123"
+accountCity  =  "indore"
+let accountState;
+
+// accountId = 123 // not allowed
+
+accountEmail =  "asf@E.COM"
+accountPassword =  "12545656"
+accountCity = "BENGLuru"
+
+
+/* use nhi krna hai var ko
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail,accountPassword,accountPassword,accountState])
+console.log(accountId)
